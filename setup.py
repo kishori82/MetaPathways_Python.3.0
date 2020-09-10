ad49556c9056af3ded2325008a88d79e2ee04f43
Kishori Mohan Konwar <kishori82@gmail.com>
2020-09-10 08:20:05 -0400

    organized the source code and fixed the module imports

--- setup.py
+++ setup.py
@@ -1,24 +1,50 @@
 import os
 from setuptools import setup
 
+CLASSIFIERS = [
+    "Development Status :: 3 - Beta",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.0+",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "MetaPathways",
-    version = "2.0.0",
-    author = "Kishori Mohan Konwar, Niels Hanson",
+    version = "3.5.0",
+    author = "Kishori Mohan Konwar",
     author_email = "kishori82@gmail.com",
     description = ("MetaPathways is a modular pipeline to build PGDBs"\
                    " from Metagenomic sequences."),
-    license = "BSD",
+    license = "MIT",
     keywords = "metagenomics pipeline",
     url = "http://packages.python.org/",
-    packages=['MetaPathways_Python'],
-    long_description=read('README'),
-    classifier = []
+    package_dir = {"": "src"},
+    packages = ["metapathways", 
+                "metapathways/pipeline",
+                "metapathways/test", 
+                "metapathways/utils",
+                "metapathways/taxonomy",
+                "metapathways/parsers",
+                "metapathways/scripts",
+                "metapathways/annotate",
+                "metapathways/diagnostics"
+    ],
+    py_modules = ['metapathways/modules'],
+    scripts = ['bin/compress_by_ec'],
+    entry_points={
+        "console_scripts": [
+            "MetaPathways = metapathways." + "MetaPathways:main"
+         ]
+    },
+    long_description=read('README.md'),
+    include_package_data=True,
+    classifiers = CLASSIFIERS
 )
